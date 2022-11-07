49d8b24c2a413db3196161c0b696545f4f9df701
Alex Power[s] BI <44716363+itsnotaboutthecell@users.noreply.github.com>
2022-11-07 17:52:39 -0600

    Updating to AzureAD module
    Updates for Tommy.

--- Power_BI_User_Licenses.ps1
+++ Power_BI_User_Licenses.ps1
@@ -1,29 +1,31 @@
-$m = "MSOnline"
 $outPath = "C:\"
 
-# Determines if final character is a forward slash, if not concatenates to the outPath variable
-if ($outPath.Substring($outPath.Length - 1, 1) -cne "\") {
-    $outPath = $outPath + "\"
-}
+#########################################
 
-# Determines if Module already exists, if not installs
-if (Get-Module -ListAvailable -Name $m) {
-        write-host "Module $m is already imported."
-    } 
-    else {
-        Install-Module -Name $m -Force -Verbose -Scope CurrentUser
-        Import-Module $m -Verbose
-}
+# Azure AD module: https://learn.microsoft.com/powershell/module/azuread/?view=azureadps-2.0
+$m = "AzureAD"
 
-Connect-MsolService
-$licenseType = "Power_BI_Pro", "Power_BI_Standard"
-$allUsers = Get-MsolUser -All | Where-Object {$_.isLicensed -eq "True"}
+try {Import-Module $m -ErrorAction Stop} 
+catch {Install-Module -Name $m -Scope CurrentUser -Force} 
+finally {Import-Module $m}
+
+Connect-AzureAD
+
+if ($outPath.Substring($outPath.Length - 1, 1) -cne "\") { $outPath = $outPath + "\" }
+if (!(Test-Path $outPath)) { New-Item -ItemType Directory -Force -Path $outPath }
+
+# Power BI Licensing Types and Capabilities Official Docs: https://docs.microsoft.com/power-bi/admin/service-admin-licensing-organization#license-types-and-capabilities
+# For a complete listing of all Licensing Service Plans visit: https://docs.microsoft.com/azure/active-directory/enterprise-users/licensing-service-plan-reference
+
+$licenseType = "POWER_BI_STANDARD","POWER_BI_ADDON","PBI_PREMIUM_P1_ADDON","PBI_PREMIUM_PER_USER","PBI_PREMIUM_PER_USER_ADDON","PBI_PREMIUM_PER_USER_DEPT","POWER_BI_PRO","POWER_BI_PRO_CE","POWER_BI_PRO_DEPT"
+
+$allUsers = Get-MsolUser -All | where {$_.isLicensed -eq "True"}
 
 foreach ($license in $licenseType) {
 
-    $Licenses = $allUsers | Where-Object {($_.licenses).AccountSkuId -match ($license)} | Select-Object objectId, WhenCreated, SignInName , displayName, Title, City, State, Country
+    $licenses = $allUsers | Where-Object {($_.licenses).AccountSkuId -match ($license)}
 
     Write-Host "Now Exporting Report: $($license)"
-    $Licenses | Export-Csv -Path "$($outPath)$($license)_$(Get-Date -Format "yyyyMMdd").csv" -NoTypeInformation
+    $licenses | Export-Csv -Path "$($outPath)$($license)_$(Get-Date -Format "yyyyMMdd").csv" -NoTypeInformation
 
 }
